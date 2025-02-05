df8f7ad1157791e880e3b3dcb336ef7d31847ff6
ElvinAlb <elvinprod@hotmail.com>
2025-02-05 16:49:38 +0100

    404 Page

--- partielreact/src/pages/404.jsx
+++ partielreact/src/pages/404.jsx
@@ -1,3 +1,10 @@
 export default function ErrorPage() {
-    return <h1>404 Error</h1>;
+    return (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWVyNnY3M3Jhczlkemtid2NkcTJ3YmJldms4OWx5bjExazR5MW5wZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9J7tdYltWyXIY/giphy.gif" alt="404"/>
+        </div>
+
+    )
+    
+    
   }
