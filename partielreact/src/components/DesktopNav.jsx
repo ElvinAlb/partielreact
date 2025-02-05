cf3316cce07319d4502b386011e7761d6a10913a
ElvinAlb <elvinprod@hotmail.com>
2025-02-05 17:10:09 +0100

    LastCommit

--- partielreact/src/components/DesktopNav.jsx
+++ partielreact/src/components/DesktopNav.jsx
@@ -3,7 +3,7 @@
 function DesktopNav() {
   return (
     <nav className="desktop-nav">
-      <a href="index.html"><i className="fas fa-film"></i></a>
+      <a href="/"><i className="fas fa-film"></i></a>
       <ul>
         <li><a href="/">Films</a></li>
         <li><a href="">Séries</a></li>
